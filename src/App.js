88a64846f781d45da47d0e433eed3d989d415b32
Daniel Frees <danielfrees@g.ucla.edu>
2023-02-25 16:56:32 -0800

    Website preview + metadata updates

--- src/App.js
+++ src/App.js
@@ -7,11 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ~~~~
           Welcome to Daniel Frees' website. 
           His AI minions are currently hard at work building the most beautiful page you've ever seen.
           Beta version will be live 03/01/2023.
-          ~~~~
         </p>
       </header>
     </div>
