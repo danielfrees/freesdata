d3b0bcd2973289c1c91f48ec48cd9a211f002b2c
Daniel Frees <danielfrees@g.ucla.edu>
2023-05-03 15:00:48 -0700

    edit tabs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './art/freesdata_logo.svg';
 import './App.css';
 import TabsComponent from './Tabs';
 
@@ -7,14 +6,6 @@ function App() {
   return (
     <div className="App">
       <TabsComponent />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Daniel Frees' website. 
-          His AI minions are currently hard at work building the most beautiful page you've ever seen.
-          Beta version will be live 06/01/2023.
-        </p>
-      </header>
     </div>
   );
 }
--- src/Tabs.js
+++ src/Tabs.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import logo from './art/freesdata_logo.svg';
 
 function TabsComponent() {
   const tabTitles = ['Tab 1', 'Tab 2', 'Tab 3'];
-  const tabContent = ['Content 1', 'Content 2', 'Content 3'];
+
+  function mainTab() {
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Welcome to Daniel Frees' website. 
+                His AI minions are currently hard at work building the most beautiful page you've ever seen.
+                Beta version will be live 06/01/2023.
+            </p>
+        </header>
+    );
+  }
+    
+  const tabContent = [<div key="mainTab">{mainTab()}</div>, 
+                    'Content 2', 
+                    'Content 3'];
 
   return (
     <Tabs>
